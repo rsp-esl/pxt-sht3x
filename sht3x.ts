fee678559f30a6c9d8f9d10a445b2757b6e19acd
rsp <siripokarpirom@gmail.com>
2019-02-20 15:15:00 +0700

    Add files via upload

--- sht3x.ts
+++ sht3x.ts
@@ -22,14 +22,14 @@ namespace SHT3X {
     }
     
     /**
-	* An SHT3X Device
-	*/
+    * An SHT3X Device
+    */
     export class Device {
         i2c_addr: number 
         err_msg: string 
         _temp: number 
         _humid: number 
-		
+        
         /**
         * Set the address of the device 
         * @param addr the new address of this device 
@@ -58,54 +58,54 @@ namespace SHT3X {
         //% weight=30 blockGap=8
         //% parts="SHT3X"
         public readData() : number [] {
-			let values: number[] = [0, 0]
+            let values: number[] = [0, 0]
             let wbuf = pins.createBuffer(2)
             this.err_msg = ''
-			
-			// single shot mode, no clock stretching
-			wbuf.setNumber( NumberFormat.UInt8LE, 0, 0x2c )
+            
+            // single shot mode, no clock stretching
+            wbuf.setNumber( NumberFormat.UInt8LE, 0, 0x2c )
             wbuf.setNumber(NumberFormat.UInt8LE, 1, 0x06)
             let result = pins.i2cWriteBuffer(this.i2c_addr, wbuf)
             if (result != 0) {
                 this.err_msg = 'I2C write failed'
                 return null
             }
-			
-			basic.pause(20)
-			
-			let buf = pins.i2cReadBuffer(this.i2c_addr, 6)
-			if (buf.length == 6) {
-				let t_hi  = buf.getNumber(NumberFormat.UInt8LE, 0)
-				let t_lo  = buf.getNumber(NumberFormat.UInt8LE, 1)
-				let t_crc = buf.getNumber(NumberFormat.UInt8LE, 2)
-				let t_value = (t_hi << 8) | t_lo
+            
+            basic.pause(20)
+            
+            let buf = pins.i2cReadBuffer(this.i2c_addr, 6)
+            if (buf.length == 6) {
+                let t_hi  = buf.getNumber(NumberFormat.UInt8LE, 0)
+                let t_lo  = buf.getNumber(NumberFormat.UInt8LE, 1)
+                let t_crc = buf.getNumber(NumberFormat.UInt8LE, 2)
+                let t_value = (t_hi << 8) | t_lo
 
-				let h_hi  = buf.getNumber(NumberFormat.UInt8LE, 3)
-				let h_lo  = buf.getNumber(NumberFormat.UInt8LE, 4)
-				let h_crc = buf.getNumber(NumberFormat.UInt8LE, 5)
-				let h_value = (h_hi << 8) | h_lo
+                let h_hi  = buf.getNumber(NumberFormat.UInt8LE, 3)
+                let h_lo  = buf.getNumber(NumberFormat.UInt8LE, 4)
+                let h_crc = buf.getNumber(NumberFormat.UInt8LE, 5)
+                let h_value = (h_hi << 8) | h_lo
 
-				if (this.crc8(t_hi, t_lo) == t_crc) { // crc ok for temperature
-					t_value  = ((t_value * 175.0) / 65535) - 45 // temperature in Celsius.
+                if (this.crc8(t_hi, t_lo) == t_crc) { // crc ok for temperature
+                    t_value  = ((t_value * 175.0) / 65535) - 45 // temperature in Celsius.
                     values[0] = t_value
                     this._temp = t_value 
                 } else {
                     this.err_msg = 'CRC failed (Temperature)'
-					return null
-				}
-				
-				if (this.crc8(h_hi, h_lo) == h_crc) { // crc ok for humidity
-					h_value =  ((h_value * 100.0) / 65535) // relative humidity
-					values[1] = h_value
+                    return null
+                }
+                
+                if (this.crc8(h_hi, h_lo) == h_crc) { // crc ok for humidity
+                    h_value =  ((h_value * 100.0) / 65535) // relative humidity
+                    values[1] = h_value
                     this._humid = h_value 
-				} else {
+                } else {
                     this.err_msg = 'CRC failed (Humidity)'
-					return null
-				} 
-				return values
-			} 
+                    return null
+                } 
+                return values
+            } 
             this.err_msg = 'I2C read failed'
-			return null
+            return null
         }  
 
         /**
@@ -138,18 +138,18 @@ namespace SHT3X {
             return this.err_msg
         }
 
-		private crc8( hi_byte: number, lo_byte: number ) : number {
-			let crc = 0xff
-			crc ^= hi_byte
-			for (let i=0; i < 8; i++) {
-				crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
-			}
-			crc ^= lo_byte
-			for (let i=0; i < 8; i++) {
-				crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
-			}
-			return crc & 0xff
-		}
+        private crc8( hi_byte: number, lo_byte: number ) : number {
+            let crc = 0xff
+            crc ^= hi_byte
+            for (let i=0; i < 8; i++) {
+                crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+            }
+            crc ^= lo_byte
+            for (let i=0; i < 8; i++) {
+                crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+            }
+            return crc & 0xff
+        }
     }
     
     /**
